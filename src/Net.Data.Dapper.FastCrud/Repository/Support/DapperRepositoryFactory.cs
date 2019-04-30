9d9a0c1ff4f5e395c0cc7345adbc410cfe2aedde
Brenno Gomes <brennogomes23@gmail.com>
2019-04-29 23:06:49 -0300

    corrigido implementacao da classe DapperRepositoryFactory

--- src/Net.Data.Dapper.FastCrud/Repository/Support/DapperRepositoryFactory.cs
+++ src/Net.Data.Dapper.FastCrud/Repository/Support/DapperRepositoryFactory.cs
@@ -1,8 +1,5 @@
 using System.Data;
 using System;
-using System.Linq;
-using System.Reflection;
-
 using Net.Data.Commons.Repository.Core;
 
 namespace Net.Data.Dapper.FastCrud.Repository.Support
@@ -18,13 +15,10 @@ namespace Net.Data.Dapper.FastCrud.Repository.Support
 
         protected override object GetCustomImplementation(Type repositoryInterface)
         {
-            var interfaces = ((TypeInfo)repositoryInterface).ImplementedInterfaces;
-            var EntityType = interfaces.First().GenericTypeArguments[0];
-            //var repositoryMetadata = GetRepositoryMetadata(repositoryInterface);
+            var repositoryMetadata = GetRepositoryMetadata(repositoryInterface);
 
             var dapperImplementationType = typeof(DapperRepository<>);
-            var customImplementationType = dapperImplementationType.MakeGenericType(EntityType);
-            //var customImplementationType = dapperImplementationType.MakeGenericType(repositoryMetadata.EntityType);
+            var customImplementationType = dapperImplementationType.MakeGenericType(repositoryMetadata.EntityType);
             var customImplementation = Activator.CreateInstance(customImplementationType, new[] { connection });
 
             return customImplementation;
